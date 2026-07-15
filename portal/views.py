bf197e3b1a13dac3606e1658674416a7a306ccaf
anushavennapusa <anushavennapusa2005@gmail.com>
2026-07-15 18:12:14 +0530

    Added about and contact views

--- portal/views.py
+++ portal/views.py
@@ -156,5 +156,6 @@ def change_password(request):
   def about(request):
     return render(request, 'about.html')
 
+
 def contact(request):
-    return render(request, 'contact.html')   
+    return render(request, 'contact.html')
